dc58a9e7db40feb8d69e5e4c118a06a3e0f8e627
codej625 <codej625@gmail.com>
2022-09-12 20:06:11 +0900

    20220912 update

--- smtp/src/main/java/com/web/smtp/SmtpController.java
+++ smtp/src/main/java/com/web/smtp/SmtpController.java
@@ -26,64 +26,55 @@ public class SmtpController {
 	static Properties mailServerProperties;
 	static MimeMessage generateMailMessage;
 
-//	@GetMapping("/")
-//	public String testController() {
-//
-//		log.info(" >> testController start");
-//
-//		return "test";
-//
-//	}
-
 	@PostMapping("/mail")
 	@ResponseBody
 	public void mailSender() throws AddressException, MessagingException {
+		
+		// sender(발신자), recipient(수신자) 
 
-		// naver smtp server를 사용한다.
-		String host = "smtp.naver.com";
+		// naver smtp server
+		String smtp_server = "smtp.naver.com";
+		// naver smtp port(ssl x)
+		int smtp_port = 465;
 
-		// naver smtp port
-		int port = 465;
-
-		// 발신자의 메일 주소
+		// sender email
 		String username = "dkwksla2@naver.com";
+		// sender password
+		final String password = "";
 
-		// 발신자의 PASSWORD
-		final String password = "!aawlsdn77";
-
-		// 수신자의 메일 주소
+		// recipient email
 		String recipient = "dkwksla@naver.com";
-
-		// 수신자에게 보낼 메일 제목
+		// 수신자에게 보낼 email subject
 		String subject = "hello! nice to meet you!";
-
-		// 수신자에게 보낼 메일 내용
+		// 수신자에게 보낼 email contents
 		String contents = "내용";
 
-		// SMTP 서버 설정 정보 세팅
+		// smtp server setting
 		Properties props = System.getProperties();
-		// smtp 서버
-		props.put("mail.smtp.host", host);
-		// smtp 포트
-		props.put("mail.smtp.port", port);
+		// smtp server
+		props.put("mail.smtp.host", smtp_server);
+		// smtp port
+		props.put("mail.smtp.port", smtp_port);
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.ssl.enable", "true");
-		props.put("mail.smtp.ssl.trust", host);
+		// ssl port 연결
+		props.put("mail.smtp.ssl.trust", smtp_server);
 
-		// Session 생성 & 발신자 smtp 서버 로그인 인증
+		// session 생성 & sender smtp server login
 		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
 			protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-				
+
 				return new javax.mail.PasswordAuthentication(username, password);
 			}
 		});
 
-		session.setDebug(true); // 디버그 모드
+		// debug mode
+		session.setDebug(true);
 
-		// MimeMessage 생성 & 메일 세팅
+		// MimeMessage 생성 & mail setting
 		Message mimeMessage = new MimeMessage(session);
-		mimeMessage.setFrom(new InternetAddress(username)); // 발신자
-		mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient)); // 수신자
+		mimeMessage.setFrom(new InternetAddress(username)); // sender
+		mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient)); // recipient
 
 		// 여러사람에게 메일을 보내고 싶을때?
 		// InternetAddress[] addArray = new InternetAddress[3];
@@ -92,13 +83,13 @@ public class SmtpController {
 		// addArray[2] = new InternetAddress("ccc@ccc.ccc");
 		// message.addRecipients(Message.RecipientType.TO, addArray);
 
-		mimeMessage.setSubject("test"); // 제목
-		mimeMessage.setText("pass"); // 내용
+		mimeMessage.setSubject("test"); // title
+		mimeMessage.setText("pass"); // contents
 
-		// 평서문이 아닌 html 태그를 보내고 싶다면?
+		// text가 아닌 html 태그를 보내고 싶다면?
 		// mimeMessage.setContent("<h1>안녕하세용?</h1>","text/html; charset=UTF-8")
 
-		Transport.send(mimeMessage); // 전송
+		Transport.send(mimeMessage); // send
 
 	}
 }
